02b628bcadf5bb9baff24087057a0ecc2ae5bd97
SageKyle <ominyipaulo@gmail.com>
2022-10-02 00:40:32 -0700

    setup dynamic content based on authentication status

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
-import { Link } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { useAuthContext } from '../hooks/useAuthContext';
 import { useLogout } from '../hooks/useLogout';
 
 // styles
@@ -6,6 +9,18 @@ import styles from './Navbar.module.css';
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+  // display notifications
+  const notify = (msg) => toast.success(msg);
+
+  const handleLogout = () => {
+    logout();
+    notify('Logout successful');
+    setTimeout(() => {
+      navigate('/');
+    }, 3000);
+  };
 
   return (
     <nav className={styles.navbar}>
@@ -13,15 +28,29 @@ export default function Navbar() {
         <li className={styles.title}>
           <Link to="/">ZenMoney</Link>
         </li>
-        <li className={styles['navbar-link']}>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li className={styles['navbar-link']}>
-          <Link to="/login">Login</Link>
-        </li>
-        <button className="btn" onClick={logout}>
-          Logout
-        </button>
+
+        {!user && (
+          <>
+            <li className={styles['navbar-link']}>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li className={styles['navbar-link']}>
+              <Link to="/login">Login</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>Hello, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={handleLogout}>
+                Logout
+              </button>
+              <Toaster />
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
