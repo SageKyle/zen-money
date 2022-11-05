a4cac000ddf16e394abe9538d359dce3161710ec
SageKyle <42160985+SageKyle@users.noreply.github.com>
2022-11-05 19:58:58 +0100

    setup protected routes

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 
 function App() {
-  const { authIsReady, user } = useAuthContext();
+	const { authIsReady, user } = useAuthContext();
 
-  return (
-    <div className="App">
-      {authIsReady && (
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </BrowserRouter>
-      )}
-    </div>
-  );
+	return (
+		<div className="App">
+			{authIsReady && (
+				<BrowserRouter>
+					<Navbar />
+					<Routes>
+						<Route path="/" element={user ? <Home /> : <Login />} />
+						<Route path="/login" element={!user ? <Login /> : <Home />} />
+						<Route path="/signup" element={!user ? <Signup /> : <Home />} />
+					</Routes>
+				</BrowserRouter>
+			)}
+		</div>
+	);
 }
 
 export default App;
